bf02738dbca13ebb297920275992e2ec0180a9f7
Kristiyan Dzhakov <kristiyandzhakov@gmail.com>
2024-02-06 18:42:25 +0000

    initial project setup

---
+++ main/map_reduce_sequential.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	mapf, reducef := loadPlugin()
+	fmt.Printf("Map function return value: %v", mapf)
+	fmt.Printf("Reduce function return value: %v", reducef)
+
+}
+
+func loadPlugin() (int, int) {
+	return 0, 0
+}
