5be01e2d41fe5873ec00cf5a5e7ffc73d015e26e
Kristiyan Dzhakov <kristiyandzhakov@gmail.com>
2024-02-08 21:28:13 +0000

    wip: read filename from input

--- main/map_reduce_sequential.go
+++ main/map_reduce_sequential.go
@@ -3,30 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"plugin"
 )
 
 // this is a duplicate struct, figure out how to use just a single one
-type KeyValue struct {
-	Key   string
-	Value string
-}
-
+// type KeyValue struct {
+// 	Key   string
+// 	Value string
+// }
 
 func main() {
-	mapf := loadPlugin()
+	fmt.Println(os.Args[1])
+	mapf := loadPlugin(os.Args[1])
 	fmt.Printf("Map function return value: %v\n", mapf)
 	// fmt.Printf("Reduce function return value: %v", reducef)
 
 }
 
-func loadPlugin() (func(string, string) []KeyValue) {
+func loadPlugin(fileName string) int {
 	/*
 		Input data will be read here;
 		The data will be passed into the Map function
 		The intermidiate data produced from Map will be accumulated and returned
 	*/
-	fileName :=  "map_reduce/map.go" // pass this as an argument
 
 	file, err := plugin.Open(fileName) // to access this, it needs to be built as plugin: go build -buildmode=plugin + path name
 
@@ -39,10 +39,11 @@ func loadPlugin() (func(string, string) []KeyValue) {
 		log.Fatalf("Cannot find map in file %v", fileName)
 	}
 
-	mapFunc := mapFuncImported.(func(string, string)[]KeyValue)
+	mapFunc := mapFuncImported
 
 	fmt.Printf("map func %v", mapFunc)
 
-	return mapFunc
+	// return mapFunc
+	return 0
 
 }
