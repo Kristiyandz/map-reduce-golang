d5a61634defb494f5e2ae8f37d83473bcfaaab88
Kristiyan Dzhakov <kristiyandzhakov@gmail.com>
2024-02-14 14:29:21 +0000

    feat: store the mapped data in intermediate result variable

--- main/main.go
+++ main/main.go
@@ -6,46 +6,44 @@ import (
 	"log"
 	"os"
 	"plugin"
-	"reflect"
 
 	"example.com/models"
 )
 
 func main() {
-	fmt.Println(os.Args[1])
-	fmt.Println("here")
+
 	mapf := loadPlugin(os.Args[1])
 
-	fmt.Printf("%v", mapf)
+	fmt.Println(os.Args[2:])
 
-	// for _, filename := range os.Args[2:] {
-	file, err := os.Open("./lotr-script.txt")
-	if err != nil {
-		log.Fatalf("cannot open %v", file)
-	}
+	intermidiate := []models.KeyValue{}
 
-	content, err := io.ReadAll(file)
-	if err != nil {
-		log.Fatalf("cannot read content %v", content)
+	// can take many file names and process them in a loop
+	// an improvement will be to send each file into a goroutine
+	for _, filename := range os.Args[2:] {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatalf("cannot open %v", file)
+		}
+
+		// whole file contents
+		content, err := io.ReadAll(file)
+		if err != nil {
+			log.Fatalf("cannot read content %v", content)
+		}
+		file.Close()
+		// pass the file name and the content
+		kva := mapf(filename, string(content)) // Pass the content as the second argument
+		intermidiate = append(intermidiate, kva...)
+
 	}
-	file.Close()
-	// kva := mapf(filename, string(1)) // Pass the content as the second argument
-	// fmt.Printf("kva: %v", kva)
-	// }
-	fmt.Printf("Map function return value: %v\n", mapf)
-	// fmt.Printf("Reduce function return value: %v", reducef)
+	// fmt.Printf("The intermidiate: %v", intermidiate)
 
 }
 
 func loadPlugin(fileName string) func(string, string) []models.KeyValue {
-	/*
-		Input data will be read here;
-		The data will be passed into the Map function
-		The intermidiate data produced from Map will be accumulated and returned
-	*/
-
-	file, err := plugin.Open(fileName) // to access this, it needs to be built as plugin: go build -buildmode=plugin + path name
-
+	// make sure this is ran: go build -buildmode=plugin + path name
+	file, err := plugin.Open(fileName)
 	if err != nil {
 		log.Fatalf("Cannot open plugin %v", fileName)
 	}
@@ -55,16 +53,10 @@ func loadPlugin(fileName string) func(string, string) []models.KeyValue {
 		log.Fatalf("Cannot find map in file %v", fileName)
 	}
 
-	fmt.Println("there")
-
 	// go run main.go ../mapreduce.so ../mapreduce/lotr-script.txt
-	// type of imported func is wrong: func(string, string) []main.KeyValue
 	mapFunc, ok := mapFuncImported.(func(string, string) []models.KeyValue)
-	fmt.Println("but not here")
-	fmt.Printf("hubabuba %v", ok)
-	if !ok {
-		log.Println(reflect.TypeOf(mapFuncImported))
 
+	if !ok {
 		log.Fatalf("Invalid type for map function in file %v", fileName)
 	}
 
--- mapreduce/mapreduce.go
+++ mapreduce/mapreduce.go
@@ -1,4 +1,4 @@
-package mapreduce
+package main
 
 import (
 	"strings"
@@ -8,13 +8,14 @@ import (
 )
 
 func Map(fileName string, contents string) []models.KeyValue {
+	// remove all symbols such as comma and periods and white space
 	ff := func(r rune) bool { return !unicode.IsLetter(r) }
-
 	words := strings.FieldsFunc(contents, ff)
 
 	keyValueSlice := []models.KeyValue{}
-
 	for _, w := range words {
+		// this is 1 so it refers to the first files passed
+		// it will increment as the number of files increases
 		kv := models.KeyValue{Key: w, Value: "1"}
 		keyValueSlice = append(keyValueSlice, kv)
 	}
